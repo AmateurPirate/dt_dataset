0819fadb626e7c275d4ecacc88c09e02420a92c1
AmateurPirate <48704991+AmateurPirate@users.noreply.github.com>
2020-06-28 10:57:09 -0400

    Add files via upload

---
+++ gen_dataset.sh
@@ -0,0 +1,7 @@
+cd /Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/youtube_dl
+
+python3 -m youtube_dl --config-location /Library/Frameworks/Python.framework/Versions/3.7/lib/python3.7/site-packages/youtube_dl
+
+cd /Users/chenzhe/pycharmprojects/dt_dataset
+
+python3 main.py -urls /Users/chenzhe/pycharmprojects/dt_dataset/dt_audio_files/dt_audio_URL_list2 -base_dir /Users/chenzhe/pycharmprojects/dt_dataset/dt_audio_files/
